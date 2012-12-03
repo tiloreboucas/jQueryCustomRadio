f2cf01e401c5472462b5f0c85f8d1f90241df4ca
OmegaMacMini <Air_i7@carlosazs-mac-mini.local>
2012-12-03 17:21:35 -0200

    Lets Rock!

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-jQueryCustomRadio
-=================
+jQueryCustomSelect
+============
 
-jQueryCustomRadioOld
+O CustomSelect tem como principal objetivo facilitar o uso e a customização de selects com uma aparencia mais agradável e mais maleável do que às oferecidas pelos SO/Navegadores, feito utilizando HTML, CSS e JavaScript, tem sua estrutura aceita por todos os navegadores atuais. além de podemos ser submetido com um form ainda pode ser navegável pelo teclado facilitando seu uso. Criado por [TiloWR](http://twitter.com/tilowr)
+
+Não deixe de visitar [TiloWR.com.br](http://www.tilowr.com.br).
+
+
+Início Rápido
+---------------
+
+Clone o repositório, `git clone git://github.com/tilowr/jQueryCustomSelect.git`, ou [baixe a última versão](https://github.com/tilowr/jQueryCustomSelect/zipball/master).
+
+
+Exemplo/Documentação
+---------
+
+http://tilowr.com.br/github/#jQueryCustomSelect
+
+
+Versionamento
+---------------
+
+Como ainda está em processo de desenvolvimento para versão BETA, ainda não tenho uma numeração de versionamento fechada, porém o objetivo será usar [Semantic Versioning 2.0.0-rc.1](http://semver.org/) 
+
+
+Bug tracker
+-------------
+
+Encontrou um BUG por favor crie um Issues aqui mesmo no GitHub
+
+https://github.com/tilowr/jQueryCustomSelect/issues
+
+
+Atualizações
+---------------
+
+Fique atento à atualizações, dê um Watch no jQuery Loader
+
+
+Desenvolvimento e Contribuições
+---------------------------------
+
+Se quiser contribuir entre em contato com [tilowr@gmail.com](tilowr@gmail.com) com o assunto jQuery CustomSelect / Contribuição 
+
+
+Licença de Uso
+--------------
---
+++ jquery.customradio.js
@@ -0,0 +1,92 @@
+(function ($) {
+    $.fn.customradio = function (method) {
+
+        var options = null;
+        var methodName = '';
+        var params = [];
+
+        if (arguments.length >= 1 && typeof (arguments[0]) == 'object')
+            options = arguments[0];
+        else if (arguments.length >= 1 && typeof (arguments[0]) == 'string') {
+            methodName = arguments[0];
+            params = arguments[1];
+        }
+
+        var attr = {
+            'target': '',
+            'buttons': {
+                'true': null,
+                'false': null,
+                'active': null
+            },
+            'content': null
+        };
+
+        var methods = {
+            init: function (options) {
+                var $this = this;
+
+                $this.attr = $.extend(true, {}, attr, options);
+                $this.attr.target = $(this);
+
+                methods.build.call($this);
+            },
+
+            build: function () {
+                var $this = this;
+
+                $this.attr.buttons.true = $this.attr.target.find("[value='true']");
+                $this.attr.buttons.false = $this.attr.target.find("[value='false']");
+
+                $this.attr.target.append("<div class='customradio_content'></div>"); 
+                $this.attr.content = $this.attr.target.find(".customradio_content");  
+                
+                $this.attr.content.append($this.attr.buttons.true);
+                $this.attr.content.append($this.attr.buttons.false);
+                
+                $this.attr.content.append("<div class='customradio_button'></div>");
+                $this.attr.buttons.active = $this.attr.target.find(".customradio_button");
+
+                if($this.attr.buttons.true[0].checked) {
+                    $this.attr.content.addClass('true');    
+                } else {
+                    $this.attr.content.addClass('false'); 
+                }
+
+                $this.attr.content.click(function(){
+                    methods.changeState.call($this);    
+                });
+            },
+
+            changeState: function(){
+                var $this = this;
+
+                if($this.attr.content.hasClass('true')){
+                    $this.attr.content.removeClass('true').addClass('false');
+                    $this.attr.buttons.true.removeAttr('checked');
+                    $this.attr.buttons.false.attr('checked', 'checked');
+                } else {
+                    $this.attr.content.removeClass('false').addClass('true');
+                    $this.attr.buttons.false.removeAttr('checked');
+                    $this.attr.buttons.true.attr('checked', 'checked');    
+                }
+            }
+        };
+
+        if (methodName != '') {
+            if (methods[methodName]) {
+                return this.each(function () {
+                    methods[methodName].call(this, params);
+                });
+            }
+            else {
+                $.error("Method '" + methodName + "' does not exist on jQuery.customradio");
+                return;
+            }
+        }
+
+        return this.each(function () {
+            methods.init.call(this, options);
+        });
+    };
+})(jQuery);
